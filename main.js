ea879acb15180aa33de3acdc209099fde6ba08b3
Lucas Gleba <lucasglebapuig@gmail.com>
2022-01-07 23:37:12 +0100

    Clean js

--- main.js
+++ main.js
@@ -3,9 +3,24 @@ const { bigInt } = require("snarkjs");
 const stringifyBigInts =
   require("websnark/tools/stringifybigint").stringifyBigInts;
 
+const MEMORY_DEPTH = 3;
 const INSTRUCTION_SIZE = 3;
 const MEMORY_SLOT_SIZE = 32;
 const TWO_POW_M_SLOT_SIZE = bigInt(2).pow(bigInt(MEMORY_SLOT_SIZE));
+
+function toPc(pc) {
+  return bigInt(pc);
+}
+
+function toMemory(code, data) {
+  return [...code, ...data].map(function (element) {
+    if (element >= 0) {
+      return bigInt(element);
+    } else {
+      return TWO_POW_M_SLOT_SIZE.sub(bigInt(Math.abs(element)));
+    }
+  });
+}
 
 function subleq(pc, mTree) {
   const insStartIndex = pc.toJSNumber() * INSTRUCTION_SIZE;
@@ -24,20 +39,6 @@ function subleq(pc, mTree) {
   }
   // mTree.update(bIndex, newMB);
   return { insStartIndex, newPc: pc, addrA, addrB, posC, mA, mB, newMB };
-}
-
-function toMemory(code, data) {
-  return [...code, ...data].map(function (element) {
-    if (element >= 0) {
-      return bigInt(element);
-    } else {
-      return TWO_POW_M_SLOT_SIZE.sub(bigInt(Math.abs(element)));
-    }
-  });
-}
-
-function toPc(pc) {
-  return bigInt(pc);
 }
 
 function step(sTree, mTree) {
@@ -101,21 +102,28 @@ function main() {
   const rawPc = 1;
   const pc = toPc(rawPc);
 
-  const memoryDepth = 3;
   const codeLen = 2; // instructions
   const codeOffset = codeLen * INSTRUCTION_SIZE;
   const code = new Array(codeOffset).fill(0);
   const pcInsStartIndex = rawPc * INSTRUCTION_SIZE;
+
   code[pcInsStartIndex + 0] = codeOffset + 0; // addrA
   code[pcInsStartIndex + 1] = codeOffset + 1; // addrB
   code[pcInsStartIndex + 2] = 0; // C
 
   const data = [10, 1]; // mA, mB
   const memory = toMemory(code, data);
-  const mTree = new MerkleTree(memoryDepth, memory);
+  const mTree = new MerkleTree(MEMORY_DEPTH, memory);
   const sTree = new MerkleTree(1, [pc, mTree.root()]);
 
   return step(sTree, mTree);
 }
 
-console.log(JSON.stringify(main(), null, 4));
+const filename = "sample";
+const fs = require("fs");
+const data = main();
+const dataStr = JSON.stringify(data, null, 4);
+fs.writeFile(filename + ".json", dataStr, function (err, result) {
+  if (err) console.log("error", err);
+});
+console.log(dataStr, `> ${filename}.json`);
