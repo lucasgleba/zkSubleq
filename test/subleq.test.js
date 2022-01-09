df9480c1ed6733dcc4a38009d7e1764b808c43ba
Lucas Gleba <lucasglebapuig@gmail.com>
2022-01-09 13:51:35 +0100

    Basic subleq testcase

--- test/subleq.test.js
+++ test/subleq.test.js
@@ -1,15 +1,28 @@
-const chai = require("chai");
-const F1Field = require("ffjavascript").F1Field;
-const Scalar = require("ffjavascript").Scalar;
-const p = Scalar.fromString(
-  "21888242871839275222246405745257275088548364400416034343698204186575808495617"
-);
+// TODO: Uninstall if not needed
+// const chai = require("chai");
+// const F1Field = require("ffjavascript").F1Field;
+// const Scalar = require("ffjavascript").Scalar;
+// const p = Scalar.fromString(
+//   "21888242871839275222246405745257275088548364400416034343698204186575808495617"
+// );
 // exports.p = p
-const Fr = new F1Field(p);
+// const Fr = new F1Field(p);
 
-const assert = chai.assert;
+// const assert = chai.assert;
 
-const { getWasmTester } = require("./utils");
+const { getWasmTester, fit2Comp } = require("./utils");
+const { bigInt } = require("snarkjs");
+
+// ERROR in plus 1? in circom etc
+
+async function checkSubleq(pcIn, aIn, bIn, cIn, circuit) {
+  const input = { pcIn, aIn, bIn, cIn };
+  const w = await circuit.calculateWitness(input, true);
+  const bOut = fit2Comp(bIn - aIn);
+  const pcOut = bOut.lesserOrEquals(bigInt.zero) ? cIn : pcIn + 1;
+  const expectedOutput = { pcOut, bOut };
+  await circuit.assertOut(w, expectedOutput);
+}
 
 describe("Test subleq circuit", function () {
   this.timeout(100000);
@@ -17,17 +30,8 @@ describe("Test subleq circuit", function () {
   before(async () => {
     circuit = await getWasmTester("lib", "subleq");
   });
-  it("ok", async () => {
-    assert(true);
+  // TODO: Fuzzing
+  it("Check test cases", async () => {
+    await checkSubleq(0, 0, 0, 1, circuit);
   });
-  // xit("Should create a iszero circuit", async () => {
-  //   let witness;
-  //   witness = await circuit.calculateWitness({ in: 111 }, true);
-  //   assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
-  //   assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)));
-
-  //   witness = await circuit.calculateWitness({ in: 0 }, true);
-  //   assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
-  //   assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)));
-  // });
 });
--- test/utils.js
+++ test/utils.js
@@ -6,6 +6,10 @@
 const { cwd } = require("process");
 const path = require("path");
 const wasm_tester = require("circom_tester").wasm;
+const { bigInt } = require("snarkjs");
+
+const NUM_SIZE = 32;
+const NUM_MAX = bigInt(2).pow(bigInt(NUM_SIZE));
 
 async function getWasmTester() {
   const circuitPath = path.join(
@@ -16,10 +20,30 @@ async function getWasmTester() {
     ...arguments,
     "circuit.circom"
   );
-  console.log(circuitPath);
+  // console.log(circuitPath);
   return await wasm_tester(circuitPath);
+}
+
+// TODO: Very different functions, might want to put them in separate files
+// Returns string
+function negTo2Comp(nn) {
+  nn = bigInt(nn);
+  if (nn.isNegative()) {
+    nn = nn.neg();
+  }
+  return NUM_MAX.sub(nn); // .toString();
+}
+
+function fit2Comp(nn) {
+  nn = bigInt(nn);
+  if (nn.isNegative()) {
+    return negTo2Comp(nn);
+  }
+  return nn; // .toString();
 }
 
 module.exports = {
   getWasmTester,
+  negTo2Comp,
+  fit2Comp,
 };
