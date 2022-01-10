aec030e59f2d441294376220958008e87028843e
Lucas Gleba <lucasglebapuig@gmail.com>
2022-01-10 21:43:29 +0100

    Add slot size opt to run.js

--- subleq_js/run.js
+++ subleq_js/run.js
@@ -2,7 +2,7 @@ const Subleq = require("./subleq");
 const StateMaker = require("./state");
 const utils = require("./utils");
 
-function run(memory0, nSteps, asciiSlots) {
+function run(memory0, nSteps, memorySlotSize, asciiSlots) {
   pc0 = 0;
   // Log base 2 of length
   const memoryDepth = Math.ceil(Math.log(memory0.length) / Math.LN2);
@@ -10,20 +10,20 @@ function run(memory0, nSteps, asciiSlots) {
   memory0 = utils.padMemory(memory0, memorySize);
 
   console.log("memory depth:", memoryDepth, "levels");
+  // console.log("memory0 size", memory0.length, "slots");
   console.log("memory size:", memorySize, "slots");
-  console.log("memory0 size", memory0.length, "slots");
   console.log("pc0:", pc0);
   console.log("");
   console.log("building trees...");
 
-  const sMaker = new StateMaker(memoryDepth, 128);
+  const sMaker = new StateMaker(memoryDepth, memorySlotSize);
   const mTree = sMaker.newMTree(memory0);
   const sTree = sMaker.newSTree(pc0, mTree);
 
   console.log(`running ${nSteps} steps...`);
   console.log("");
 
-  const subleq = new Subleq(128);
+  const subleq = new Subleq(memorySlotSize);
   for (let ss = 0; ss < nSteps; ss++) {
     if (ss % 5 == 0) {
       console.log("step", ss);
@@ -48,15 +48,16 @@ function run(memory0, nSteps, asciiSlots) {
 }
 
 function main() {
-  let [filepath, nSteps, asciiSlots] = process.argv.slice(2, 5);
+  let [filepath, nSteps, memorySlotSize, asciiSlots] = process.argv.slice(2, 6);
   const memory0 = utils.readMemoryFile(filepath);
   nSteps = nSteps || 1;
+  memorySlotSize = memorySlotSize || 128;
   if (asciiSlots != undefined) {
     asciiSlots = asciiSlots.split(",");
   } else {
     asciiSlots = [];
   }
-  run(memory0, nSteps, asciiSlots);
+  run(memory0, nSteps, memorySlotSize, asciiSlots);
 }
 
 if (require.main === module) {
